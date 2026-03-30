24bb19af8bb65f33ae55193793150f1422f69415
Tet Elite <5hibalord69@gmail.com>
2026-03-30 08:10:37 +0700

    Task W10 02: add forceFetch parameter to fetchSongs method and implement caching in SongRepositoryFirebase

--- lib/data/repositories/songs/song_repository_firebase.dart
+++ lib/data/repositories/songs/song_repository_firebase.dart
@@ -11,8 +11,19 @@ class SongRepositoryFirebase extends SongRepository {
 
   late final Uri songsUri = Uri.https(baseHost, '/songs.json');
 
+  List<Song>? _cachedSongs;
+
   @override
-  Future<List<Song>> fetchSongs() async {
+  Future<List<Song>> fetchSongs({bool forceFetch = false}) async {
+    if (!forceFetch && _cachedSongs != null) {
+      return _cachedSongs!;
+    }
+    final songs = await _fetchSongsFromApi();
+    _cachedSongs = songs;
+    return songs;
+  }
+
+  Future<List<Song>> _fetchSongsFromApi() async {
     final http.Response response = await http.get(songsUri);
 
     if (response.statusCode == 200) {
@@ -42,7 +53,15 @@ class SongRepositoryFirebase extends SongRepository {
     );
 
     if (response.statusCode == 200 || response.statusCode == 204) {
-      return song.copyWith(likes: song.likes + 1);
+      final updatedSong = song.copyWith(likes: song.likes + 1);
+
+      if (_cachedSongs != null) {
+        _cachedSongs = _cachedSongs!.map((s) {
+          return s.id == song.id ? updatedSong : s;
+        }).toList();
+      }
+
+      return updatedSong;
     } else {
       throw Exception('Failed to like song ${song.id}');
     }
